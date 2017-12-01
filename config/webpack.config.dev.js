e35a6f8f6ab71e146b372f4e8420f9b7f9251602
Matt Wilson <matt.wilson@immediate.co.uk>
2017-12-01 17:07:04 +0000

    Added eslint to webpack build.

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -5,32 +5,43 @@ todo:
 - configure eslint
 */
 
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
-const srcDir  = path.resolve(__dirname, '../src/js');
+const srcDir = path.resolve(__dirname, '../src/js');
 const buildDir = path.resolve(__dirname, '../build');
 
 const config = {
   context: srcDir,
   entry: {
-    app: './index.js'
+    app: './index.js',
   },
   output: {
     path: buildDir,
     publicPath: '/',
-    filename: 'static/[name].[chunkhash:8].js'
+    filename: 'static/[name].[chunkhash:8].js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'eslint-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: [{
-          loader: 'babel-loader'
-        }]
+          loader: 'babel-loader',
+        }],
       },
       {
         test: /\.scss$/,
@@ -39,44 +50,44 @@ const config = {
             {
               loader: 'css-loader',
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
               loader: 'postcss-loader',
               options: {
                 ident: 'postcss',
                 plugins: () => [
-                  require('autoprefixer')(),
-                  require('cssnano')()
+                  autoprefixer(),
+                  cssnano(),
                 ],
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
               loader: 'sass-loader',
               options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      }
-    ]
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   devtool: 'cheap-module-source-map',
   plugins: [
     new ExtractTextPlugin({
-      filename: 'static/[name].[contenthash:8].css'
+      filename: 'static/[name].[contenthash:8].css',
     }),
     new HtmlWebpackPlugin({
       inject: true,
       template: path.resolve(__dirname, '../index.html'),
     }),
-  ]
-}
+  ],
+};
 
 module.exports = config;
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -8,15 +8,15 @@ const config = merge(devConfig, {
   devtool: 'none',
   plugins: [
     new CleanWebpackPlugin(['build'], {
-      root: process.cwd()
+      root: process.cwd(),
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
 });
 
 module.exports = config;
